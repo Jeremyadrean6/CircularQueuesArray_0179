25a7cd0dafd56a7a750a57c0fb3fc112faf37e47
Jeremyadrean6 <bangje43121@gmail.com>
2025-06-12 11:18:33 +0700

    menambahkan REAR jika berada di akhir array, array kembali ke awal

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -36,4 +36,14 @@ public:
             REAR = 0;  // 2.b
         }
         else
-     
+        {
+            // jika REAR berada di posisi terakhir Array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+   
+    }
+}
