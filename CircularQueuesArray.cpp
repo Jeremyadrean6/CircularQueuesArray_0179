a989b851064545900df071cb43c70f9b312a7ff3
Jeremyadrean6 <bangje43121@gmail.com>
2025-06-12 11:27:45 +0700

    jika FRONT_position, iterasi front hingga rear

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -84,5 +84,15 @@ public:
 
         cout << "\nElements in the queue are...\n";
 
+        // Jika FRONT_positif,, iterasi dari FRONT hingga rear
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+        else
     }
-}  
+}
