1df4fdbf245797c9fcc5342d4a31a46e64dc9404
Jeremyadrean6 <bangje43121@gmail.com>
2025-06-12 11:19:45 +0700

    menambahkan void remove

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -46,4 +46,15 @@ public:
         queue_array[REAR] = num;
    
     }
+
+    void remove (){
+        // cek apakah antrian kosng 
+        if (FRONT == -1 ){
+            cout << "Queue underflow\n ";
+            return;
+        }
+        cout << "\nThe.element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+
+    }  
 }
