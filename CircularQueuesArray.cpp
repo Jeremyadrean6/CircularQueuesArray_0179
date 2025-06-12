c6e86b2c54f8000c3fd60591763b942398da8e21
Jeremyadrean6 <bangje43121@gmail.com>
2025-06-12 11:23:05 +0700

    jika elment ang dihapus berada  di akhir array , kembali ke awal

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -62,5 +62,12 @@ public:
             REAR = -1;  
         }
         else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir Array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 }
